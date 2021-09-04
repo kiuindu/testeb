247a00d2782a5afe4ca379bb6dda19e0c2ca7e76
kiuind <kiuind@gmail.com>
2021-09-04 16:46:36 -0300

    your message

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+console.log("testebb")
 const socket = io('http://localhost:3000')
+console.log("teste")
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
